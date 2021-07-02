56a7fb8cb3bd59235482819b87b1e97edb5d43d5
avinashkafle0 <avinashkafle0@gmail.com>
2021-07-02 13:54:42 +0545

    project initializaiton

---
+++ src/main/java/com/ticketreservation/TicketReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ticketreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketReservationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketreservation/TicketReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
